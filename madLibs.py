2d27ed58005dae65c73ea5ff51c9551b5fcd118a
Koki <kgoudy64@gmail.com>
2020-08-30 21:38:59 -0500

    Create madLibs.py
    Trying my hand at a mad libs game

---
+++ madLibs.py
@@ -0,0 +1,7 @@
+color = blue
+smell = jasmine
+music = JPop
+
+print("My favorite color is " + color)
+print("I really enjoy the smell of " + smell)
+print("I really have come to enjoy " + music + " music")
