cdc4ac9797fe84824dd11b51717a6954593f5df1
Koki <kgoudy64@gmail.com>
2020-10-03 09:58:28 -0500

    Update madLibs.py

--- madLibs.py
+++ madLibs.py
@@ -1,6 +1,6 @@
-color = blue
-smell = jasmine
-music = JPop
+color = "blue"
+smell = "jasmine"
+music = "K-Pop"
 
 print("My favorite color is " + color)
 print("I really enjoy the smell of " + smell)
