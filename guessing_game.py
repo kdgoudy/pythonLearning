eda961a354656a751fabd9541a72c549f46ee3aa
Koki <kgoudy64@gmail.com>
2020-10-03 21:55:27 -0500

    Create guessing_game.py
    Creating simple word guessing game

---
+++ guessing_game.py
@@ -0,0 +1,10 @@
+#Creat secret word
+secret_word = "sword"
+guess = ""
+
+#Create while loop
+while guess != secret_word:
+  guess = input("Enter your guess: ")
+  
+#Create print message for you guess correctly
+print("You win!")
