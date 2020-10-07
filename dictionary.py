7a4680729524a2330266b54db853ef13c3d723cc
Koki <kgoudy64@gmail.com>
2020-10-06 19:59:05 -0500

    Create dictionary.py
    Creating a dictionary and calling a specific item from the dictionary.

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+#Creating a dictionary using the months of the year
+monthConversions = {
+  "Jan": "January",
+  "Feb": "February",
+  "Mar": "March",
+  "Apr": "April",
+  "May": "May",
+  "Jun": "June",
+  "Jul": "July",
+  "Aug": "August",
+  "Sep": "September",
+  "Oct": "October",
+  "Nov": "November",
+  "Dec": "December",
+}
+
+#Call a specific month from the dictionary
+print(monthConversions["Aug"])
