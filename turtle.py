4c9c6219f9a21bf699f370e781a9e8209f930307
Koki <kgoudy64@gmail.com>
2020-10-13 21:10:06 -0500

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+koki_turtle = turtle.Turtle()
+koki_turtle.forward(100)
+koki_turtle.right(90)
+koki_turtle.forward(90)
+koki_turtle.right(90)
+koki_turtle.forward(100)
+koki_turtle.right(90)
+koki_turtle.forward(100)
