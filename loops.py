1d1d0a023f912ceeb2e74a4c21db8cddf2a48283
Koki <kgoudy64@gmail.com>
2020-10-13 20:59:07 -0500

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+#while loop
+a = 0
+
+while a <= 20:
+  print(a)
+  a += 2
+  
+  
+#for loop
+fruits = ['apple', 'banana', 'orange', 'pear', 'grape', 'strawberry', 'kiwi']
+
+for x in fruits:
+  print(x)
